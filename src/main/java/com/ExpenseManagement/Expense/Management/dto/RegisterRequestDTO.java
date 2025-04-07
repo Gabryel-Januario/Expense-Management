98ebae11662e4fbeec0e3c919999a90fc4b0fe65
Gabryel Januario <gabryeelcomy@gmail.com>
2025-04-07 07:54:59 -0300

    fix: change name to RegisterRequestDTO to make more sense

---
+++ src/main/java/com/ExpenseManagement/Expense/Management/dto/RegisterRequestDTO.java
@@ -0,0 +1,27 @@
+package com.ExpenseManagement.Expense.Management.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class RegisterRequestDTO {
+    
+    @NotBlank(message = "name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "login cannot be empty")
+    private String login;
+
+    @NotBlank(message = "password cannot be empty")
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
